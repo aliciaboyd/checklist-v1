9303e8b2321a12fafc24cd76f10bcaf8b14cc0cf
Alicia Boyd <26505495+aliciaboyd@users.noreply.github.com>
2023-03-13 23:32:36 -0400

    Adds local storage to checklist

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # checklist-v1
 Simple checklist in javascript
+
+---
+
+## To Do
+1. Build simple checklist ✅
+1. Add dynamic date ✅
+2. Add local storage to persist sessions ✅
+3. Save state of checkbox between sessions ⬜
--- scripts/script.js
+++ scripts/script.js
@@ -1,21 +1,70 @@
-const userInputField = document.querySelector('#textbox');
-const addItemButton = document.querySelector('#submit');
-const listGroup = document.querySelector('.list-group');
-let listItems = [];
-let storage = localStorage.getItem("checklist-items");
+const userInputField = document.querySelector("#textbox");
+const addItemButton = document.querySelector("#submit");
+const listGroup = document.querySelector(".list-group");
+let listItems;
 
-
-const deleteItemButton = document.querySelectorAll('.delete');
-
-// 1. create storage array 
-// 2. iterate through storage array, create elements for page
-// 3. add delete button
-//    3.5 delete button filters value of element and removes from array.
-
-function newListItem() {
+if (!localStorage.getItem("checklist")) {
+  listItems = [];
+} else {
+  listItems = JSON.parse(localStorage.getItem("checklist"));
 }
 
-addItemButton.addEventListener('click', (event) => {
+function onLoadList(list) {
+  if (list.length < 1) {
+    return;
+  }
+
+  for (let i = 0; i < list.length; i++) {
+    addElement(list[i]);
+  }
+}
+
+function newListItem(input) {
+  if (input.value === "") {
+    return;
+  }
+
+  let value = input.value;
+
+  listItems.push(value);
+  localStorage.setItem("checklist", JSON.stringify(listItems));
+
+  addElement(value);
+  input.value = "";
+}
+
+function addElement(value) {
+  let listElement = document.createElement("li");
+  listElement.classList.add("list-item");
+
+  let checkBox = document.createElement("input");
+  checkBox.type = "checkbox";
+
+  let toDoParagraph = document.createElement("p");
+  toDoParagraph.innerHTML = value;
+
+  let deleteButton = document.createElement("button");
+  deleteButton.classList.add("delete");
+  deleteButton.ariaLabel = "Delete Item";
+
+  listElement.appendChild(checkBox);
+  listElement.appendChild(toDoParagraph);
+  listElement.appendChild(deleteButton);
+  listGroup.appendChild(listElement);
+
+  deleteButton.addEventListener("click", () => {
+    let currentStorage = JSON.parse(localStorage.getItem("checklist"));
+    listItems = currentStorage.filter((element) => element != value);
+
+    localStorage.setItem("checklist", JSON.stringify(listItems));
+    listGroup.removeChild(listElement);
+  });
+}
+
+addItemButton.addEventListener("click", (event) => {
   event.preventDefault();
   newListItem(userInputField);
 });
+
+//renders local storage list on load
+onLoadList(listItems);
